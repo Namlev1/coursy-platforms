427aab331e62fb7f1c49b90b866489b3a1276466
namlev <mikipatkowski@protonmail.com>
2025-03-28 13:47:24 +0100

    Add test HomeController with helloWorld endpoint

---
+++ src/main/kotlin/com/coursy/be/HomeController.kt
@@ -0,0 +1,14 @@
+package com.coursy.be
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1")
+class HomeController {
+    @GetMapping
+    fun helloWorld(): String {
+        return "Hello world!"
+    }
+}
