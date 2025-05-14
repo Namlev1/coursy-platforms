11d51ccfd6dc35f70bd7fb3647fe5d6793fdc5ba
namlev <mikipatkowski@protonmail.com>
2025-05-14 12:39:25 +0200

    Make PlatformFailure implement Failure

--- src/main/kotlin/com/coursy/masterservice/failure/PlatformFailure.kt
+++ src/main/kotlin/com/coursy/masterservice/failure/PlatformFailure.kt
@@ -1,5 +1,9 @@
 package com.coursy.masterservice.failure
 
-sealed class PlatformFailure {
+sealed class PlatformFailure : Failure {
     data class NotFound(val id: Long) : PlatformFailure()
+
+    override fun message(): String = when (this) {
+        is NotFound -> "Platform with id=${id} was not found"
+    }
 }
