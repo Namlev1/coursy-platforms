9275f7ab17876d77fc237552fa92df4400be9d11
namlev <mikipatkowski@protonmail.com>
2025-06-14 19:20:26 +0200

    Remove unused dtos

--- src/main/kotlin/com/coursy/platforms/dto/JwtResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.coursy.platforms.dto
-
-data class JwtResponse(
-    val token: String,
-    val refreshToken: String,
-)
--- src/main/kotlin/com/coursy/platforms/dto/LoginRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.coursy.platforms.dto
-
-data class LoginRequest(
-    val email: String,
-    val password: String
-)
